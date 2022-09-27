c88e5f2e148f075f0ca3c11e344ef90a45d1392d
nayyeay <nayla1694@gmail.com>
2022-09-27 13:31:29 +0700

    pwpb_270922_basic JS Chapter 1

---
+++ commentVariabel_1.js
@@ -0,0 +1,36 @@
+//single line comment
+/*
+multiple line commet
+first chapter is addition
+*/
+var x = 23.5;
+var y = 24.5;
+var addition = x+y;
+console.log(addition)
+
+/*
+multiple line commet
+second chapter is substration
+*/
+var x = 26;
+var y = 22;
+var addition = x-y;
+console.log(addition)
+
+/*
+multiple line commet
+third chapter is division
+*/
+var x = 24;
+var y = 28;
+var addition = x/y;
+console.log(addition)
+
+/*
+multiple line commet
+foutrh chapter is multiplication
+*/
+var x = 20;
+var y = 4;
+var addition = x*y;
+console.log(addition)
---
+++ home.js
@@ -0,0 +1,4 @@
+//var hello = "Hello World";
+//console.log(Hello)
+//console.log('Hello Selamat Datang)
+console.log('Hello World')
