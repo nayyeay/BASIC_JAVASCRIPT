35979b54d2932fc5469fe6c703c0ab25dce578d7
nayyeay <nayla1694@gmail.com>
2022-10-22 11:15:32 +0700

    18-10-2022

---
+++ Module4Quiz_29.2_1.js
@@ -0,0 +1,8 @@
+function test(number)
+{
+   while(number < 5) {
+      number++;
+   }
+   return number;
+}
+console.log(test(2));
---
+++ Module4Quiz_29.2_3.js
@@ -0,0 +1,10 @@
+function max(a, b) {
+    if (a >= b)
+    
+    return a;
+    else
+    
+    return b;
+    
+    }
+    
